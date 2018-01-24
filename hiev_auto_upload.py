635a49ee5cec69fd9e61cabbb446c2dac7830a65
Gerard Devine <g.devine@westernsydney.edu.au>
2018-01-24 15:53:09 +1100

    Updated stanza for yaml file

--- hiev_auto_upload.py
+++ hiev_auto_upload.py
@@ -3,7 +3,7 @@ Python script to perform automated data uploads into the HIEv application.
 
 Using this script, a user can dump files (with a file naming convention applied) into the 
 Data directory and use the file_settings.yaml file to specify regex matching patterns against 
-those files. Upin successful upload into HIEv the matched files are moved out of the Data directory
+those files. Upon successful upload into HIEv the matched files are moved out of the Data directory
 and into the Backups folder.   
 
 This script expects a folder named 'Data' and a folder named 'Backups' at the same level as this 
